d56b8ed17d21e1f901a8d1dc8a4b5faf81c09840
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-03-22 22:01:35 +0545

    fix: Correct file path for login logs extraction

--- src/extract_logs.py
+++ src/extract_logs.py
@@ -1,8 +1,9 @@
-import pandas as pd 
+import pandas as pd
 
 def extract_logs():
     print("Extracting logs...")
 
-    file_path ="user_login_log_pipeline/data/raw/login_logs.csv"
+    file_path = "../data/raw/login_logs.csv"
     df = pd.read_csv(file_path)
+
     return df
